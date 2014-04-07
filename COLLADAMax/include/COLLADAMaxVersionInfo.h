98bf6ed00223b5d1e2bac5a6d176d9eb4f752b12
bagobor <bagobor@gmail.com>
2014-04-08 01:14:36 +0400

    -last changes

--- COLLADAMax/include/COLLADAMaxVersionInfo.h
+++ COLLADAMax/include/COLLADAMaxVersionInfo.h
@@ -6,8 +6,8 @@
 namespace COLLADAMax 
 { 
     const String CURRENT_REVISION = "Unversioned"; 
-    const String CURRENT_PLATFORM = "x64"; 
-    const String CURRENT_CONFIGURATION = "Release_Max2013_static"; 
+    const String CURRENT_PLATFORM = "Win32"; 
+    const String CURRENT_CONFIGURATION = "Debug_Max2008_static"; 
 } 
   
 #endif // __COLLADAMAX_VERSIONINFO_H__ 
